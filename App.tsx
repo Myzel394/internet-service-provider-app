5a848cbcc83eafc71abcdad9f4428347aa51b37d
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-09-11 21:40:27 +0200

    current stand

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
+import 'react-native-gesture-handler';
 import AppBody from "./src/widgets/AppBody";
 import ProfileInformation from "./src/widgets/ProfileInformation";
 import {View} from "react-native";
 import QuickSelections from "./src/widgets/QuickSelections";
+import BottomSheet from '@gorhom/bottom-sheet';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {useRef} from "react";
 
 export default function App() {
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
     return (
-        <AppBody>
-            <View style={{height: 40}}/>
-            <ProfileInformation/>
-            <QuickSelections/>
-        </AppBody>
+        <GestureHandlerRootView>
+            <AppBody>
+                <View style={{height: 40}}/>
+                <ProfileInformation/>
+                <QuickSelections/>
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    snapPoints={["25%"]}
+                    index={0}
+                >
+                    <View style={{backgroundColor: "red", flex: 1}}/>
+                </BottomSheet>
+            </AppBody>
+        </GestureHandlerRootView>
     );
 }
