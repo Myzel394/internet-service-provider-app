3c787aa6c23b239662044649e356f82a9ff56598
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-09-11 21:01:00 +0200

    improved IconButton.tsx

--- src/widgets/IconButton.tsx
+++ src/widgets/IconButton.tsx
@@ -1,19 +1,24 @@
 import {ReactElement} from "react";
-import {StyleSheet, TouchableOpacity} from "react-native";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import useSelectThemeStyleSheet from "../hooks/use-select-theme-stylesheet";
 
 export interface IconButtonProps {
     icon: ReactElement;
     onPress: () => void;
+
+    subTitle?: string;
 }
 
-export default function IconButton({icon, onPress}: IconButtonProps): ReactElement {
+export default function IconButton({icon, onPress, subTitle}: IconButtonProps): ReactElement {
     const styles = useSelectThemeStyleSheet(lightStyles, darkStyles);
 
     return (
-        <TouchableOpacity onPress={onPress} style={[baseStyles.icon, styles.icon]}>
-            {icon}
-        </TouchableOpacity>
+        <View style={baseStyles.wrapper}>
+            <TouchableOpacity onPress={onPress} style={[baseStyles.icon, styles.icon]}>
+                {icon}
+            </TouchableOpacity>
+            {subTitle && <Text style={[baseStyles.subTitle, styles.subTitle]}>{subTitle}</Text>}
+        </View>
     );
 }
 
@@ -23,19 +28,34 @@ const baseStyles = StyleSheet.create({
         borderRadius: 18,
         aspectRatio: 1,
         alignSelf: "center",
-        elevation: 2,
+        elevation: 4,
     },
+    wrapper: {
+        flexDirection: "column",
+    },
+    subTitle: {
+        fontSize: 14,
+        fontWeight: "400",
+        textAlign: "center",
+        marginTop: 8,
+    }
 });
 
 const lightStyles = StyleSheet.create({
     icon: {
         backgroundColor: "#CCCCCC",
-    }
+    },
+    subTitle: {
+        color: "#888888",
+    },
 });
 
 const darkStyles = StyleSheet.create({
     icon: {
         backgroundColor: "#303030",
         color: "white"
-    }
+    },
+    subTitle: {
+        color: "#5A5A5A",
+    },
 });
