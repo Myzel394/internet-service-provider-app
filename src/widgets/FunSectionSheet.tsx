415c8f07729382bf9864f4b58f9211e5d041245f
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-09-13 18:54:09 +0200

    improved backdrop

--- src/widgets/FunSectionSheet.tsx
+++ src/widgets/FunSectionSheet.tsx
@@ -1,7 +1,6 @@
-import BottomSheet from "@gorhom/bottom-sheet";
 import BlurView from "expo-blur/build/BlurView";
 import React, {ReactElement} from "react";
-import {StyleSheet, Text} from "react-native";
+import {Text} from "react-native";
 import ModalSheet from "./ModalSheet";
 
 export interface FunSelectionSheetProps {
@@ -18,8 +17,11 @@ export default function FunSectionSheet({visible, onClose}: FunSelectionSheetPro
             snapPoints={SNAP_POINTS}
             index={visible ? 0 : -1}
             backdropComponent={() =>
-                <BlurView style={{flex: 1, position: "absolute", width: "100%", height: "100%"}} intensity={20}>
-                </BlurView>
+                visible ? <BlurView
+                    style={{flex: 1, position: "absolute", width: "100%", height: "100%"}}
+                    intensity={20}
+                    tint="dark"
+                /> : null
             }
             onClose={onClose}
             enablePanDownToClose
